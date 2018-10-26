778b51c0f1f355e75015d5fb4bc120e335ee06bd
Ramon Mota <rmluzz@gmail.com>
2018-10-26 17:39:14 -0300

    Cofiguração Inicial

---
+++ MorePopularMoviesApp/app/src/main/java/org/themoviedb/api/morepopularmoviesapp/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package org.themoviedb.api.morepopularmoviesapp.ui
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import org.themoviedb.api.morepopularmoviesapp.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
